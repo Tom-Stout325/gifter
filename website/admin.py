8f3aca8f1e460047f45ca821adfc47061cf4358e
Tom Stout <tom.stout325@gmail.com>
2023-12-28 23:40:11 -0500

    Added models

--- website/admin.py
+++ website/admin.py
@@ -10,7 +10,17 @@ class ProfileAdmin(admin.ModelAdmin):
 class FamilyAdmin(admin.ModelAdmin):
     list_display = ['name']
 
+class GiftAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+class HobbyAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
 
 admin.site.register(Family, FamilyAdmin)
 
 admin.site.register(Profile, ProfileAdmin)
+
+admin.site.register(Gift, GiftAdmin)
+
+admin.site.register(Hobby, HobbyAdmin)
--- website/models.py
+++ website/models.py
@@ -15,12 +15,41 @@ class Family(models.Model):
 
 
 
+class Gift(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=100)
+    qty = models.IntegerField()
+    color = models.CharField(max_length=100)
+    size = models.CharField(max_length=100)
+    store = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    created = models.DateField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateField(auto_now=True, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+
+class Hobby(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return str(self.name)
+    
+    class Meta:
+        verbose_name_plural = "Hobbies"
+
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
     family_name = models.ForeignKey(Family, on_delete=models.DO_NOTHING)
-    birthday = models.DateField(blank=True, null=True)
-    anniversary = models.DateField(blank=True, null=True)
+    birthday = models.DateField(auto_now_add=False, blank=True, null=True)
+    anniversary = models.DateField(auto_now_add=False, blank=True, null=True)
     avatar = models.ImageField(upload_to='avatars/', default='media/avatars/default_user.png')
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username} {self.user.first_name}" + f"{self.family_name}"
